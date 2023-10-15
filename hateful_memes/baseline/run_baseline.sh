5362c90f641833c3a1fd9de94a8b02cc1b76fe06
galkowskim <mikolaj.galkowski7@gmail.com>
2023-10-15 20:51:21 +0200

    update run script

--- hateful_memes/baseline/run_baseline.sh
+++ hateful_memes/baseline/run_baseline.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH --account=ganzha_23
-#SBATCH --job-name=run_gen
+#SBATCH --job-name=run_gen_baseline
 #SBATCH --partition=short
 #SBATCH --cpus-per-task=8
-#SBATCH --gpus-per-task=8
-#SBATCH --mem=256G
+#SBATCH --gpus-per-task=4
+#SBATCH --mem=20G
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mikolaj.galkowski.stud@pw.edu.pl
-#SBATCH --output=/home2/faculty/mgalkowski/logs/baseline
+#SBATCH --output=/home2/faculty/mgalkowski/logs/baseline/baseline1.log
 
+. /home2/faculty/mgalkowski/miniconda3/etc/profile.d/conda.sh
 conda activate baseline
 
-python main.py --config_path config.json --data_dir ../../data/hateful_memes --log_dir "../logs/baseline" --model_dir "../model/baseline"
+python main.py --config_path config.json --data_dir ../../data/hateful_memes --log_dir "../logs/baseline"
