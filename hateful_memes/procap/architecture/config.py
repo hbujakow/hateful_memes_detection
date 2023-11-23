19c40325265d47c4013f45f4ff9d1723217c1d6b
galkowskim <mikolaj.galkowski7@gmail.com>
2023-11-23 11:48:44 +0100

    fix type

--- hateful_memes/procap/architecture/config.py
+++ hateful_memes/procap/architecture/config.py
@@ -61,7 +61,7 @@ def parse_opt():
 
     parser.add_argument("--DEBUG", type=bool, default=False)
     parser.add_argument("--SAVE", type=bool, default=True)
-    parser.add_argument("--SAVE_NUM", type=int, default=datetime.now().strftime("%Y%m%d_%H%M"))
+    parser.add_argument("--SAVE_NUM", type=str, default=datetime.now().strftime("%Y%m%d_%H%M"))
     parser.add_argument("--EPOCHS", type=int, default=10)
 
     parser.add_argument("--SEED", type=int, default=1111, help="random seed")
