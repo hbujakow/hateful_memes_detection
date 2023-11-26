a34be9d29374a229d8d0d496683ab1ef58e7cee8
Wiktor Jakubowski <72559388+WJakubowsk@users.noreply.github.com>
2023-11-26 20:41:58 +0100

    Add OCR & Inpainting API to the inference (#4)
    * add image inpainting
    
    * update inpainting
    
    * reformat deepfillv2 and add header
    
    * add inpainter demo
    
    * add ocr
    
    * add inpainter api
    
    * update README
    
    * add download model instruction
    
    * update inpainting API
    
    * add environment config
    
    * Update __init__.py
    
    * Update README.md
    
    * Update README.md
    
    * Rename download_pretrained_models.md to README.md
    
    * Update README.md
    
    * Update .gitignore
    
    * resolve issues
    
    * Update README.md
    
    * Refactor ImageConverter class and update app.py
    
    * update inpainting module
    
    ---------
    
    Co-authored-by: hbujakow <h.bujakow@gmail.com>
    Co-authored-by: Wiktor Jakubowski <wjakubowski@eden@mini.pw.edu.pl>
    Co-authored-by: galkowskim <mikolaj.galkowski7@gmail.com>

--- hateful_memes/inpainting/app.py
+++ hateful_memes/inpainting/app.py
@@ -1,33 +1,40 @@
+import base64
+from io import BytesIO
+
 import uvicorn
 from fastapi import FastAPI, HTTPException
+from PIL import Image
 from pydantic import BaseModel
 
-# TODO - LOAD INPAINTING MODEL
-model = ...
+from Inpainter import ImageConverter
 
 app = FastAPI()
+img_converter = ImageConverter()
 
 
 class InputData(BaseModel):
-    """Use this data model to parse the input data JSON request body."""
+    """Data model for input data to API"""
 
-    ...
+    image: str
 
 
 @app.post("/inpaint")
-async def predict(data: InputData):
-    # TODO - IMPLEMENT THIS ENDPOINT
-    # MAYBE IMAGE NEED TO BE ENCODED IN BASE64 FOR TRANSMISSION
+async def predict(image: InputData):
+    im = Image.open(BytesIO(base64.b64decode((image.image))))
+    img_converter.upload_img(image = im)
     try:
-        features = ...
+        inpainted_image = img_converter.inpaint_image()
+        buffered = BytesIO()
+        inpainted_image.save(buffered, format="PNG")
+        image_bytes = buffered.getvalue()
+        encoded_image = base64.b64encode(image_bytes).decode("utf-8")
 
-        image = model.predict(features)
-
-        return {"image": image}
+        text = img_converter.retrieve_text()
+        return {"image": encoded_image, "text": text}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=8089)
