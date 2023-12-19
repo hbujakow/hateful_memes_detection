40deadf75a96929318339e4dc2a7ad65ac6d6643
WJakubowsk <wiktor.jak97@gmail.com>
2023-12-19 23:56:14 +0100

    update inference

--- hateful_memes/llava/evaluation/generate_predictions.sh
+++ hateful_memes/llava/evaluation/generate_predictions.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #SBATCH --account=ganzha_23
-#SBATCH --partition=long
+#SBATCH --partition=long,short
 #SBATCH --cpus-per-gpu=4
-#SBATCH --gpus=a100:3
+#SBATCH --gpus=a100:4
 #SBATCH --time=24:00:00
-#SBATCH --mem=150G
+#SBATCH --mem=50G
 #SBATCH --ntasks=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wiktor.jakubowski.stud@pw.edu.pl
-#SBATCH --job-name=simple_inference
-#SBATCH --output=/home2/faculty/wjakubowski/logs/llava/finetuned_inference.log
+#SBATCH --job-name=inference
+#SBATCH --output=/home2/faculty/wjakubowski/logs/llava/finetuned_inference_run_inference_train.log
 
 . /home2/faculty/wjakubowski/miniconda3/etc/profile.d/conda.sh
 conda activate llava
 
 export PYTHONPATH=/home2/faculty/wjakubowski/memes_analysis/hateful_memes/llava/LLaVA/
 
-python evaluate_llava.py
+python run_inference.py \
+    --test-file "/home2/faculty/wjakubowski/memes_analysis/data/dev_seen.jsonl" \
+    --dest-file "/home2/faculty/wjakubowski/memes_analysis/data/dev_seen_with_predictions.json"
--- hateful_memes/llava/evaluation/run_inference.py
+++ hateful_memes/llava/evaluation/run_inference.py
@@ -17,9 +17,10 @@ from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
 from llava.mm_utils import (
+    process_images,
     tokenizer_image_token,
     get_model_name_from_path,
-    # KeywordsStoppingCriteria,
+    KeywordsStoppingCriteria,
 )
 
 from PIL import Image
@@ -46,7 +47,6 @@ def get_chunk(lst, n, k):
     chunks = split_list(lst, n)
     return chunks[k]
 
-
 def predict(args) -> None:
     """
     Runs inference on a pretrained model to make predictions on test data.
@@ -62,72 +62,74 @@ def predict(args) -> None:
     )
 
     df = pd.read_json(args.test_file, lines=True)
-    prompt = args.prompt
 
-    for idx, row in tqdm(df.iterrows()):
-        image_file = row["img"]
-        if model.config.mm_use_im_start_end:
-            prompt = (
+    if model.config.mm_use_im_start_end:
+            question = (
                 DEFAULT_IM_START_TOKEN
                 + DEFAULT_IMAGE_TOKEN
                 + DEFAULT_IM_END_TOKEN
                 + "\n"
-                + prompt
+                + args.question
             )
-        else:
-            prompt = DEFAULT_IMAGE_TOKEN + "\n" + prompt
+    else:
+        question = DEFAULT_IMAGE_TOKEN + "\n" + args.question
 
-        conv = conv_templates[args.conv_mode].copy()
-        conv.append_message(conv.roles[0], prompt)
-        conv.append_message(conv.roles[1], None)
-        prompt = conv.get_prompt()
+    conv = conv_templates[args.conv_mode].copy()
+    conv.append_message(conv.roles[0], question)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
 
-        input_ids = (
-            tokenizer_image_token(
-                prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
-            )
-            .unsqueeze(0)
-            .cuda()
+    input_ids = (
+        tokenizer_image_token(
+            prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
         )
+        .unsqueeze(0)
+        .cuda()
+    )
 
+    input_token_len = input_ids.shape[1]
+
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+    keywords = [stop_str]
+    stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+
+    for image_file in tqdm(df["img"]):
         image = Image.open(os.path.join(args.data_folder, image_file))
-        image_tensor = image_processor.preprocess(image, return_tensors="pt")[
-            "pixel_values"
-        ][0]
-
-        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
-        # keywords = [stop_str]
-        # stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+        image_tensor = process_images(
+            [image],
+            image_processor,
+            model.config
+        ).to(model.device, dtype=torch.float16)
 
         with torch.inference_mode():
             output_ids = model.generate(
                 input_ids,
-                images=image_tensor,  # .unsqueeze(0).half().cuda(),
+                images=image_tensor.unsqueeze(0).half().cuda(),
                 do_sample=True if args.temperature > 0 else False,
                 temperature=args.temperature,
                 top_p=args.top_p,
                 num_beams=args.num_beams,
                 max_new_tokens=args.max_new_tokens,
                 use_cache=True,
+                stopping_criteria=[stopping_criteria],
             )
-
-        input_token_len = input_ids.shape[1]
+        
         n_diff_input_output = (
-            (input_ids != output_ids[:, :input_token_len]).sum().item()
+        (input_ids != output_ids[:, :input_token_len]).sum().item()
         )
         if n_diff_input_output > 0:
             print(
                 f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
             )
-        outputs = tokenizer.batch_decode(
+
+        output = tokenizer.batch_decode(
             output_ids[:, input_token_len:], skip_special_tokens=True
         )[0]
-        outputs = outputs.strip()
-        if outputs.endswith(stop_str):
-            outputs = outputs[: -len(stop_str)].strip()
+        output = output.strip()
+        if output.endswith(stop_str):
+            output = output[: -len(stop_str)].strip()
 
-        df.at[idx, "prediction"] = outputs
-        df.at[idx, "label"] = 1 if outputs == "Yes" else 0
+        df.loc[df["img"] == image_file, "prediction"] = output
 
     df.to_json(args.dest_file)
 
@@ -139,7 +141,7 @@ if __name__ == "__main__":
     parser.add_argument("--data-folder", type=str, default="/home2/faculty/wjakubowski/memes_analysis/data/")
     parser.add_argument("--test-file", type=str, default="/home2/faculty/wjakubowski/memes_analysis/data/dev_seen.jsonl")
     parser.add_argument("--dest-file", type=str, default="/home2/faculty/wjakubowski/memes_analysis/data/dev_seen_predictions_10_epochs.json")
-    parser.add_argument("--prompt", type=str, default="Is this meme hateful?\nAnswer the question using a single word or phrase.")
+    parser.add_argument("--question", type=str, default="Is this meme hateful?\nAnswer the question using a single word or phrase.")
     parser.add_argument("--conv-mode", type=str, default="llava_v1")
     parser.add_argument("--num-chunks", type=int, default=1)
     parser.add_argument("--chunk-idx", type=int, default=0)
