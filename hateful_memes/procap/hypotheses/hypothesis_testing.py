2d7973cff283dd8cc1446f398078f70169d25d00
WJakubowsk <wiktor.jak97@gmail.com>
2024-01-13 23:26:36 +0100

    add map to numpy

--- hateful_memes/procap/hypotheses/hypothesis_testing.py
+++ hateful_memes/procap/hypotheses/hypothesis_testing.py
@@ -5,10 +5,10 @@ from de_long_test import delong_roc_test
 
 
 def main(args):
-    data = pd.read_jsonl(args.datapath_path, lines=True)
+    data = pd.read_json(args.datapath_path, lines=True)
     true_labels = data[args.labels_name].values
-    probas_model1 = torch.load(args.probas_first_model_path)
-    probas_model2 = torch.load(args.probas_second_model_path)
+    probas_model1 = torch.load(args.probas_first_model_path).cpu()[:, 1]
+    probas_model2 = torch.load(args.probas_second_model_path).cpu()[:, 1]
 
     p_value = delong_roc_test(true_labels, probas_model1, probas_model2)
     print(
@@ -21,26 +21,26 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "tested_functionality",
+        "--tested_functionality",
         type=str,
         choices=["inpainting", "distillation"],
         help="Functionality of proposed solution to test.",
     )
     parser.add_argument(
-        "--datapath-path",
+        "--data-path",
         type=str,
-        default="/home2/faculty/wjakubowski/memes_analysis/data/llava_preds",
+        default="/home2/faculty/wjakubowski/memes_analysis/data/dev_seen.jsonl",
     )
-    parser.add_argument("labels-name", type=str, default="label")
+    parser.add_argument("--labels-name", type=str, default="label")
     parser.add_argument(
         "--probas-first-model-path",
         type=str,
-        default="/home2/faculty/wjakubowski/memes_analysis/data/llava_preds",
+        default="/home2/faculty/wjakubowski/memes_analysis/data/probs/probs_query_vinai_bertweet_large_epochs_15_inpainted.pkl",
     )
     parser.add_argument(
         "--probas-second-model-path",
         type=str,
-        default="/home2/faculty/wjakubowski/memes_analysis/data/llava_preds",
+        default="/home2/faculty/wjakubowski/memes_analysis/data/probs/probs_query_vinai_bertweet_large_epochs_15_not_inpainted.pkl",
     )
     args = parser.parse_args()
 
