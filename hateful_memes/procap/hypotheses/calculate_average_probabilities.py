f20bf696218e0df147cb426b4312da93fec0cafc
WJakubowsk <wiktor.jak97@gmail.com>
2024-01-14 20:25:11 +0100

    add averaging predictions

--- hateful_memes/procap/hypotheses/calculate_average_probabilities.py
+++ hateful_memes/procap/hypotheses/calculate_average_probabilities.py
@@ -1,52 +1,45 @@
 import os
+import re
 import pickle
 import argparse
 import numpy as np
 import torch
 from typing import List
 
-def get_subdirectories(directory: str):
-    return [subdir for subdir in os.listdir(directory) if os.path.isdir(os.path.join(directory, subdir))]
+def get_model_types(directory: str):
+    return ['_'.join(subdir.split('_', 2)[2:]) for subdir in os.listdir(directory) if os.path.isdir(os.path.join(directory, subdir))]
 
 def calculate_average_probs(input_data_dir: str, output_data_dir: str, model_type_list: List[str], inpainting_type: str, seed_list: List[int]):
     """
     Calculates tensor of average probabilities for each model trained with several different seeds.
     Saves the results to respective pytorch tensor files.
     """
-    avg_probs = np.zeros(2)
+    avg_probs = {model: None for model in model_type_list}
     num_seeds = len(seed_list)
 
 
     for model_type in model_type_list:
         for seed in seed_list:
-            seed_folder = os.path.join(input_data_dir, f"seed_{seed}", model_type)
-
-            inpainted_probs_file = f"probs_{seed_folder.split('/')[-1]}.pkl"
-            not_inpainted_probs_file = f"probs_query_{seed_folder.split('/')[-1]}.pkl"
-
-            inpainted_probs_path = os.path.join(seed_folder, inpainted_probs_file)
-            not_inpainted_probs_path = os.path.join(seed_folder.replace("inpainted", "not_inpainted"), not_inpainted_probs_file)
-
-            if os.path.exists(inpainted_probs_path) and os.path.exists(not_inpainted_probs_path):
-                with open(inpainted_probs_path, 'rb') as f:
-                    inpainted_probs = pickle.load(f)
-
-                with open(not_inpainted_probs_path, 'rb') as f:
-                    not_inpainted_probs = pickle.load(f)
-
-                avg_probs[0] += inpainted_probs
-                avg_probs[1] += not_inpainted_probs
-
-        avg_probs /= num_seeds
-        
+            seed_folder = os.path.join(input_data_dir, f"seed_{seed}", inpainting_type)
+            for model_folder in os.listdir(seed_folder):
+                if model_type in model_folder:
+                    for file in os.listdir(os.path.join(seed_folder, model_folder)):
+                        if bool(re.compile(r"probs_\d+").search(file)): # checks for file with probabilities
+                            with open(os.path.join(seed_folder, model_folder, file), 'rb') as f:
+                                probs = torch.load(f, map_location=torch.device('cpu'))[:, 1]
+                            if avg_probs[model_type] is None:
+                                avg_probs[model_type] = probs
+                            else:
+                                avg_probs[model_type] += probs
+        avg_probs[model_type] /= num_seeds
         output_file = os.path.join(output_data_dir, inpainting_type, model_type +  "_avg_probs.pt")
-        torch.save(torch.tensor(avg_probs), output_file)
+        torch.save(torch.tensor(avg_probs[model_type]), output_file)
 
 def main(args):
     input_data_dir = args.input_data_dir
     output_data_dir = args.output_data_dir
     seed_list = [1111, 2222, 3333, 4444, 5555]
-    model_type_list = get_subdirectories(input_data_dir + "/seed_1111/inpainted")
+    model_type_list = get_model_types(input_data_dir + "/seed_1111/inpainted")
 
     inpainted_avg_probs = calculate_average_probs(input_data_dir, output_data_dir, model_type_list, "inpainted", seed_list)
     not_inpainted_avg_probs = calculate_average_probs(input_data_dir, output_data_dir, model_type_list, "not_inpainted", seed_list)
@@ -56,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input-data-dir",
         type=str,
-        default="/home2/faculty/mgalkowski/memes_analysis/hateful_memes/procap/architecture/logits_probs",
+        default="/home2/faculty/wjakubowski/memes_analysis/data/procap_probs",
     )
     parser.add_argument(
         "--output-data-dir",
