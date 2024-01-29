66a6cca4541cac9236ec9e45177724a492e90bea
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2024-01-29 03:55:26 +0100

    add containerization

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -4,15 +4,10 @@ provider "azurerm" {
 
 
 locals {
-  function_app_settings = {
-    PROCAP_API_URL = azurerm_linux_function_app.classification_api.default_hostname
-    CAPTION_API_URL = azurerm_linux_function_app.captioning_api.default_hostname
-    INPAINT_API_URL = azurerm_linux_function_app.inpainting_api.default_hostname
-  }
   env_variables = {
    DOCKER_REGISTRY_SERVER_URL            = "https://memescontainerregistry.azurecr.io"
-   DOCKER_REGISTRY_SERVER_USERNAME       = "memescontainerregistry"
-   DOCKER_REGISTRY_SERVER_PASSWORD       = "PTJS8WnKSV1IjXeoO94c9E6Xz7TCTD6o0kHhQYFa5m+ACRDmH5BG"
+   DOCKER_REGISTRY_SERVER_USERNAME       = "" # fill it in
+   DOCKER_REGISTRY_SERVER_PASSWORD       = "" # fill it in
  }
 }
 
@@ -21,63 +16,6 @@ resource "azurerm_resource_group" "memes-resource-group" {
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "memes_aps" {
-  name                = "memes-appservice-plan"
-  location            = azurerm_resource_group.memes-resource-group.location
-  resource_group_name = azurerm_resource_group.memes-resource-group.name
-  sku_name            = "Y1"
-  os_type             = "Linux"
-}
-
-resource "azurerm_linux_web_app" "memes_app_service" {
-  name                  = "memes-app-service"
-  location              = azurerm_resource_group.memes-resource-group.location
-  resource_group_name   = azurerm_resource_group.memes-resource-group.name
-  service_plan_id       = azurerm_service_plan.memes_aps.id
-  app_settings          = local.function_app_settings
-
-  site_config {
-    # always_on           = true
-    # linux_fx_version    = "DOCKER|memescontainerregistry.azurecr.io/hateful_memes_app:latest"
-  }
-}
-
-resource "azurerm_linux_function_app" "captioning_api" {
-  name                = "memes-captioning-function-app"
-  location            = azurerm_resource_group.memes-resource-group.location
-  resource_group_name = azurerm_resource_group.memes-resource-group.name
-  # service_plan_id = azurerm_service_plan.api_memes_aps.id
-  service_plan_id = azurerm_service_plan.memes_aps.id
-
-  storage_account_name = azurerm_storage_account.memes_storage_account.name
-  storage_account_access_key = azurerm_storage_account.memes_storage_account.primary_access_key
-  site_config {
-  }
-}
-
-resource "azurerm_linux_function_app" "inpainting_api" {
-  name                = "memes-inpainting-function-app"
-  location            = azurerm_resource_group.memes-resource-group.location
-  resource_group_name = azurerm_resource_group.memes-resource-group.name
-  service_plan_id = azurerm_service_plan.memes_aps.id
-
-  storage_account_name = azurerm_storage_account.memes_storage_account.name
-  storage_account_access_key = azurerm_storage_account.memes_storage_account.primary_access_key
-  site_config {
-  }
-}
-
-resource "azurerm_linux_function_app" "classification_api" {
-  name                = "memes-classification-function-app"
-  location            = azurerm_resource_group.memes-resource-group.location
-  resource_group_name = azurerm_resource_group.memes-resource-group.name
-  service_plan_id = azurerm_service_plan.memes_aps.id
-
-  storage_account_name = azurerm_storage_account.memes_storage_account.name
-  storage_account_access_key = azurerm_storage_account.memes_storage_account.primary_access_key
-  site_config {
-  }
-}
 
 resource "azurerm_storage_account" "memes_storage_account" {
   name                     = "sahatefulmemes"
@@ -85,4 +23,98 @@ resource "azurerm_storage_account" "memes_storage_account" {
   location                 = azurerm_resource_group.memes-resource-group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_log_analytics_workspace" "memes_log_analytics_workspace" {
+  name                = "memes-log-analytics-workspace"
+  location            = azurerm_resource_group.memes-resource-group.location
+  resource_group_name = azurerm_resource_group.memes-resource-group.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "memes_app_environment" {
+    name                       = "memes-app-environment"
+    location                   = azurerm_resource_group.memes-resource-group.location
+    resource_group_name        = azurerm_resource_group.memes-resource-group.name
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.memes_log_analytics_workspace.id
+}
+
+resource "azurerm_container_app" "hateful_memes_classifier" {
+    name                         = "haetful-memes-classifier"
+    container_app_environment_id = azurerm_container_app_environment.memes_app_environment.id
+    resource_group_name          = azurerm_resource_group.memes-resource-group.name
+    revision_mode                = "Single"
+
+    template {
+        container {
+        name   = "haetful-memes-classifier-app"
+        image  = "memescontainerregistry.azurecr.io/hateful_memes_app:latest"
+        cpu    = 0.25
+        memory = "0.5Gi"
+        }
+  }
+}
+
+resource "azurerm_container_app" "inpainting_api_container" {
+    name                         = "inpainting-api"
+    container_app_environment_id = azurerm_container_app_environment.memes_app_environment.id
+    resource_group_name          = azurerm_resource_group.memes-resource-group.name
+    revision_mode                = "Single"
+
+    template {
+        container {
+        name   = "haetful-memes-inpainting-api-container"
+        image  = "memescontainerregistry.azurecr.io/hateful_memes_inpainting_api:latest"
+        cpu    = 2
+        memory = "4Gi"
+        }
+  }
+}
+# Blip2 model needs to be deployed in AzureML
+resource "azurerm_container_app" "captioning_api_container" { 
+    name                         = "captioning-api"
+    container_app_environment_id = azurerm_container_app_environment.memes_app_environment.id
+    resource_group_name          = azurerm_resource_group.memes-resource-group.name
+    revision_mode                = "Single"
+
+    template {
+        container {
+        name   = "hateful-memes-captioning-api-container"
+        image  = "memescontainerregistry.azurecr.io/hateful_memes_captioning_api:latest"
+        cpu    = 0.25
+        memory = "0.5Gi"
+        }
+  }
+}
+
+# workspace for RoBERTa and Blip2 model
+
+resource "azurerm_key_vault" "memes_key_vault" {
+  name                = "memes-key-vault"
+  location            = azurerm_resource_group.memes-resource-group.location
+  resource_group_name = azurerm_resource_group.memes-resource-group.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_application_insights" "memes_ai_insights" {
+  name                = "memes-ai-insights"
+  location            = azurerm_resource_group.memes-resource-group.location
+  resource_group_name = azurerm_resource_group.memes-resource-group.name
+  application_type    = "web"
+}
+
+resource "azurerm_machine_learning_workspace" "memes-ml-workspace" {
+    name                    = "memes-ml-workspace"
+    location                = azurerm_resource_group.memes-resource-group.location
+    resource_group_name     = azurerm_resource_group.memes-resource-group.name
+    application_insights_id = azurerm_application_insights.memes_ai_insights.id
+    storage_account_id      = azurerm_storage_account.memes_storage_account.id
+    key_vault_id            = azurerm_key_vault.memes_key_vault.id
+    # container_registry_id = local.env_variables.DOCKER_REGISTRY_SERVER_URL
+
+    identity {
+        type = "SystemAssigned"
+    }
 }
